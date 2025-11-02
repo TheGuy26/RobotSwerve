791490fe12e4ae36e509f436445ab419590e9f28
nadav <guyyadgar26@gmail.com>
2025-11-02 20:54:12 +0200

    continued with Swerve class

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -5,16 +5,34 @@ import frc.robot.Math.Vector2d;
 import frc.robot.Utils.EverKit.EverGyro;
 
 public class Swerve extends SubsystemBase{
+    private static Swerve m_swerve;
+    
     private EverGyro gyro;
 
-
     private Vector2d driveVector;
-    private Vector2d steerVector;
 
-    public void drive(Vector2d leftStickVector)
+    private SwerveModule[] swerveModules;
+
+    public Swerve()
     {
+        
+    }
+
+    public void drive(Vector2d leftStickVector, double rightStickVectorVel)
+    {
+        // set drive vector
         driveVector = leftStickVector.copy().mul(swerveConst.MAX_SPEED);
         driveVector.rotate(Math.toRadians(gyro.getYaw()) * swerveConst.GYRO_DIRECTION); // gyro axis is false
+
+        // update rotation vectors
+        for (Vector2d rotationVector : swerveConst.MODULES_POSITIONS) {
+            rotationVector.mul(rightStickVectorVel * convertToMPerSec(swerveConst.MAX_ANGULAR_SPEED));
+        }
+
+        // apply vector to module
+        for (int i = 0; i < swerveModules.length; i++) {
+            swerveModules[i].set(driveVector.copy().add(swerveConst.MODULES_POSITIONS[i]));
+        }
     }
 
     public static void setModuleVectores(Vector2d moduleVectors[]) {
@@ -22,5 +40,10 @@ public class Swerve extends SubsystemBase{
             moduleVector.normalise();
             moduleVector.rotate(Math.toRadians(90));
         }
+    }
+
+    public static double convertToMPerSec(double degPerSec) {
+        double ciclePerSec = degPerSec / 360;
+        return ciclePerSec * swerveConst.ROBOT_ROUNDING_CICLE_PERIMETER;
     }
 }
--- src/main/java/frc/robot/subsystems/swerveConst.java
+++ src/main/java/frc/robot/subsystems/swerveConst.java
@@ -1,14 +1,95 @@
 package frc.robot.subsystems;
 
 import frc.robot.Math.Vector2d;
+import frc.robot.Utils.EverKit.EverAbsEncoder;
+import frc.robot.Utils.EverKit.Implementations.Encoders.EverCANCoder;
+import frc.robot.Utils.EverKit.Implementations.Encoders.EverSparkInternalEncoder;
+import frc.robot.Utils.EverKit.Implementations.Encoders.EverTalonFXInternalEncoder;
+import frc.robot.Utils.EverKit.Implementations.MotorControllers.EverSparkMax;
+import frc.robot.Utils.EverKit.Implementations.MotorControllers.EverTalonFX;
+import frc.robot.Utils.EverKit.Implementations.PIDControllers.EverSparkMaxPIDController;
+import frc.robot.Utils.EverKit.Implementations.PIDControllers.EverTalonFXPIDController;
+
+/*
+ * TL = Top Left
+ * TR = Top Right
+ * DL = Down Left
+ * DR = Down Right
+ */
 
 public interface swerveConst {
-    public static final double MAX_SPEED = 2;
+    public static final double MAX_SPEED = 2; // meter per sec
+    public static final double MAX_ANGULAR_SPEED = 180; // deg per sec
+
+    // motor controllers
+    public static final EverTalonFX 
+            TL_DRIVE_MOTOR = new EverTalonFX(18),
+            TR_DRIVE_MOTOR = new EverTalonFX(16), 
+            DL_DRIVE_MOTOR = new EverTalonFX(1),  
+            DR_DRIVE_MOTOR = new EverTalonFX(3); 
+    
+    public static final EverSparkMax 
+            TL_STEER_MOTOR = new EverSparkMax(11),
+            TR_STEER_MOTOR = new EverSparkMax(17),
+            DL_STEER_MOTOR = new EverSparkMax(10),
+            DR_STEER_MOTOR = new EverSparkMax(2);
+
+    public static final EverTalonFX[] DRIVE_MOTORS = {TL_DRIVE_MOTOR, TR_DRIVE_MOTOR, DL_DRIVE_MOTOR, DR_DRIVE_MOTOR};
+    public static final EverSparkMax[] STEER_MOTORS = {TL_STEER_MOTOR, TR_STEER_MOTOR, DL_STEER_MOTOR, DR_STEER_MOTOR};
+
+    // encoders
+    public static final EverTalonFXInternalEncoder 
+            TL_DRIVE_ENCODER = new EverTalonFXInternalEncoder(TL_DRIVE_MOTOR),
+            TR_DRIVE_ENCODER = new EverTalonFXInternalEncoder(TR_DRIVE_MOTOR),
+            DL_DRIVE_ENCODER = new EverTalonFXInternalEncoder(DL_DRIVE_MOTOR),
+            DR_DRIVE_ENCODER = new EverTalonFXInternalEncoder(DR_DRIVE_MOTOR);
+
+    public static final EverSparkInternalEncoder 
+            TL_STEER_ENCODER = new EverSparkInternalEncoder(TL_STEER_MOTOR),
+            TR_STEER_ENCODER = new EverSparkInternalEncoder(TR_STEER_MOTOR),
+            DL_STEER_ENCODER = new EverSparkInternalEncoder(DL_STEER_MOTOR),
+            DR_STEER_ENCODER = new EverSparkInternalEncoder(DR_STEER_MOTOR);
+
+    public static final EverTalonFXInternalEncoder[] DRIVE_ENCODERS = {TL_DRIVE_ENCODER, TR_DRIVE_ENCODER, DL_DRIVE_ENCODER, DR_DRIVE_ENCODER};
+    public static final EverSparkInternalEncoder[] STEER_ENCODERS = {TL_STEER_ENCODER, TR_STEER_ENCODER, DL_STEER_ENCODER, DR_STEER_ENCODER};
+
+    // swerve module pid controllers
+    public static final EverTalonFXPIDController 
+            TL_VELOCITY_CONTROLLER = new EverTalonFXPIDController(TL_DRIVE_MOTOR),
+            TR_VELOCITY_CONTROLLER = new EverTalonFXPIDController(TR_DRIVE_MOTOR),
+            DL_VELOCITY_CONTROLLER = new EverTalonFXPIDController(DL_DRIVE_MOTOR),
+            DR_VELOCITY_CONTROLLER = new EverTalonFXPIDController(DR_DRIVE_MOTOR);
+    
+    public static final EverSparkMaxPIDController                 
+            TL_ANGLE_CONTROLLER = new EverSparkMaxPIDController(TL_STEER_MOTOR),
+            TR_ANGLE_CONTROLLER = new EverSparkMaxPIDController(TR_STEER_MOTOR),
+            DL_ANGLE_CONTROLLER = new EverSparkMaxPIDController(DL_STEER_MOTOR),
+            DR_ANGLE_CONTROLLER = new EverSparkMaxPIDController(DR_STEER_MOTOR);
+    
+    public static final EverTalonFXPIDController[] WHEEL_VELOCITY_CONTROLLERS = {TL_VELOCITY_CONTROLLER, TR_VELOCITY_CONTROLLER, DL_VELOCITY_CONTROLLER, DR_VELOCITY_CONTROLLER};
+    public static final EverSparkMaxPIDController[] WHEEL_ANGLE_CONTROLLERS = {TL_ANGLE_CONTROLLER, TR_ANGLE_CONTROLLER, DL_ANGLE_CONTROLLER, DR_ANGLE_CONTROLLER};
+    
+    // chassis encoders 
+    public static final EverAbsEncoder
+            TL_ABS_ENCODER = new EverCANCoder(1),
+            TR_ABS_ENCODER = new EverCANCoder(0),
+            DL_ABS_ENCODER = new EverCANCoder(3),
+            DR_ABS_ENCODER = new EverCANCoder(2);
+
+    public static final EverAbsEncoder[] ABS_ENCODERS = {TL_ABS_ENCODER, TR_ABS_ENCODER, DL_ABS_ENCODER, DR_ABS_ENCODER};
+
+    // swerve module velocity pidf values
+    public static final double WHEEL_VELOCITY_KP = 0.1, WHEEL_VELOCITY_KI = 0.0, WHEEL_VELOCITY_KD = 0.00,
+            WHEEL_VELOCITY_KV = 1/8.5, WHEEL_VELOCITY_KS = 0;
+    // swerve module wheel angle pid values
+    public static final double WHEEL_ANGLE_KP = 0.01, WHEEL_ANGLE_KI = 0.0, WHEEL_ANGLE_KD = 0.000;
+
+
 
     // motor gear ratios
     public static final double
-                DRIVE_MOTOR_RAIO = 6.75,
-                STEER_MOTOR_RATIO = 12.8;
+                DRIVE_GEAR_RAIO = 1 / 6.75,
+                STEER_GEAR_RATIO = 1 / 12.8;
     
     public static final double WHEEL_PERIMETER = Math.PI * 0.09;
 
@@ -17,11 +98,14 @@ public interface swerveConst {
     //chassis dimentions
     public static final double CHASSIS_LENTGH = 0.75, CHASSIS_WIDTH = 0.75;
 
+    public static final double ROBOT_ROUNDING_CICLE_PERIMETER = Math.PI * Math.sqrt(CHASSIS_LENTGH * CHASSIS_LENTGH + CHASSIS_WIDTH * CHASSIS_WIDTH);
+
     public static final Vector2d
-                        TR_VEC = new Vector2d(CHASSIS_WIDTH / 2, CHASSIS_LENTGH / 2),
                         TL_VEC = new Vector2d(-CHASSIS_WIDTH / 2, CHASSIS_LENTGH / 2),
-                        DR_VEC = new Vector2d(CHASSIS_WIDTH / 2, -CHASSIS_LENTGH / 2),
-                        DL_VEC = new Vector2d(-CHASSIS_WIDTH / 2, -CHASSIS_LENTGH / 2);
+                        TR_VEC = new Vector2d(CHASSIS_WIDTH / 2, CHASSIS_LENTGH / 2),
+                        DL_VEC = new Vector2d(-CHASSIS_WIDTH / 2, -CHASSIS_LENTGH / 2),
+                        DR_VEC = new Vector2d(CHASSIS_WIDTH / 2, -CHASSIS_LENTGH / 2);
+                        
     
-    public static final Vector2d[] SWERVE_MODULES = { TR_VEC, TL_VEC, DR_VEC, DL_VEC };
+    public static final Vector2d[] MODULES_POSITIONS = { TL_VEC, TR_VEC, DL_VEC, DR_VEC};
 }
