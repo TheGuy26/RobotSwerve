81128cdfbb9c7b9b2376c9d425ec4e120fc88d85
nadav <guyyadgar26@gmail.com>
2025-11-02 18:45:06 +0200

    swerve drive & modules vectors

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -6,8 +6,21 @@ import frc.robot.Utils.EverKit.EverGyro;
 
 public class Swerve extends SubsystemBase{
     private EverGyro gyro;
+
+
+    private Vector2d driveVector;
+    private Vector2d steerVector;
+
     public void drive(Vector2d leftStickVector)
     {
+        driveVector = leftStickVector.copy().mul(swerveConst.MAX_SPEED);
+        driveVector.rotate(Math.toRadians(gyro.getYaw()) * swerveConst.GYRO_DIRECTION); // gyro axis is false
+    }
 
+    public static void setModuleVectores(Vector2d moduleVectors[]) {
+        for (Vector2d moduleVector : moduleVectors) {
+            moduleVector.normalise();
+            moduleVector.rotate(Math.toRadians(90));
+        }
     }
 }
--- src/main/java/frc/robot/subsystems/swerveConst.java
+++ src/main/java/frc/robot/subsystems/swerveConst.java
@@ -1,11 +1,27 @@
 package frc.robot.subsystems;
 
-public interface swerveConst {
-    public static final double MAXSPEED = 1.5;
+import frc.robot.Math.Vector2d;
 
+public interface swerveConst {
+    public static final double MAX_SPEED = 2;
+
+    // motor gear ratios
     public static final double
-                driveMotorRatio = 6.75,
-                steerMotorRatio = 12.8;
+                DRIVE_MOTOR_RAIO = 6.75,
+                STEER_MOTOR_RATIO = 12.8;
     
     public static final double WHEEL_PERIMETER = Math.PI * 0.09;
+
+    public int GYRO_DIRECTION = -1; // -1 is clockwise
+
+    //chassis dimentions
+    public static final double CHASSIS_LENTGH = 0.75, CHASSIS_WIDTH = 0.75;
+
+    public static final Vector2d
+                        TR_VEC = new Vector2d(CHASSIS_WIDTH / 2, CHASSIS_LENTGH / 2),
+                        TL_VEC = new Vector2d(-CHASSIS_WIDTH / 2, CHASSIS_LENTGH / 2),
+                        DR_VEC = new Vector2d(CHASSIS_WIDTH / 2, -CHASSIS_LENTGH / 2),
+                        DL_VEC = new Vector2d(-CHASSIS_WIDTH / 2, -CHASSIS_LENTGH / 2);
+    
+    public static final Vector2d[] SWERVE_MODULES = { TR_VEC, TL_VEC, DR_VEC, DL_VEC };
 }
